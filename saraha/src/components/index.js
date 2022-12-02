a444fccee1e6b0f53a22c95946ad17fc0dddbba4
sahar <saharabdalazeez97@gmail.com>
2022-12-03 00:05:14 +0200

    packages

--- saraha/src/components/index.js
+++ saraha/src/components/index.js
@@ -2,5 +2,6 @@ import {Input} from './Input';
 import {Screen} from './Screen';
 import {Logo} from './Logo';
 import {MainButton} from './MainButton';
+import {NavigationButton} from './NavigationButton';
 
-export {Input, Screen, Logo, MainButton};
+export {Input, Screen, Logo, MainButton, NavigationButton};
