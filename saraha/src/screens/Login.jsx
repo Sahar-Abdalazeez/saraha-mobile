20cd860dfd1efb0851f8e6f6fa7ad232ccea2c36
sahar <saharabdalazeez97@gmail.com>
2022-12-03 00:05:03 +0200

    login

--- saraha/src/screens/Login.jsx
+++ saraha/src/screens/Login.jsx
@@ -1,27 +1,42 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+//icons
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope'
 import { faPerson } from '@fortawesome/free-solid-svg-icons/faPerson'
-import { faLock } from '@fortawesome/free-solid-svg-icons/faLock'
-
+//components
 import { View, Text, StyleSheet } from 'react-native';
-import { Screen, Input, Logo, MainButton } from '../components';
+import { Screen, Input, Logo, MainButton, NavigationButton } from '../components';
+//storage 
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+/**
+ * Login
+ */
 export const Login = () => {
+    //handle login 
+    const handleLogin = async () => {
+        try {
+            const savedUser = await AsyncStorage.getItem("user");
+            const currentUser = JSON.parse(savedUser);
+            console.log(currentUser);
+        } catch (error) {
+            console.log(error);
+        }
+    };
     return (
 
         <Screen style={styles.screen}>
+            <NavigationButton text='Register' />
+
             <View style={styles.container}>
                 <Logo style={{ alignSelf: 'center' }} />
                 <Text style={styles.title}>Sarahah</Text>
 
                 <Input icon={faPerson} placeholder="Username" />
                 <Input icon={faEnvelope} placeholder="Email" />
-                <Input icon={faLock} placeholder="Password" />
-                <Input icon={faLock} placeholder="Confirm Password" />
 
-                <MainButton text="Register" />
             </View>
+            <MainButton onPress={() => handleLogin()} text="Login" />
+
         </Screen>
     )
 }
@@ -29,15 +44,15 @@ export const Login = () => {
 
 const styles = StyleSheet.create({
     screen: {
-
-        paddingTop: 50
-
+        paddingTop: 30,
+        padding: 0
     },
     container: {
         disply: 'flex',
         flex: 1,
         width: '100%',
         height: '100%',
+        padding: 20
     },
     title: {
         fontSize: 50,
