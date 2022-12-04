a4d103ff7ab21d81630f4b288ef30a5324e895bd
sahar <saharabdalazeez97@gmail.com>
2022-12-05 00:43:18 +0200

    users and send message ready

--- saraha/src/screens/SendMessage.jsx
+++ saraha/src/screens/SendMessage.jsx
@@ -1,7 +1,87 @@
-import { Text } from 'react-native';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
+//icon
+import { faPen } from "@fortawesome/free-solid-svg-icons/faPen";
+//compnents 
+import { Text, StyleSheet, View, TextInput, SafeAreaView, StatusBar } from 'react-native';
+import { Header, IconButton, Modal } from '../components';
 
-export const SendMessage = () => {
+export const SendMessage = ({ navigation, route }) => {
+    const { id, name } = route.params;
+    const [message, setMessage] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    console.log('id,name', id, name)
+    //send message function
+    const handleSend = async () => {
+        let { data } = await axios.post(
+            `http://10.0.2.2:3000/api/v1/message/${id}`,
+            { text: message }
+        );
+        console.log('======data', data);
+        if (data.message == "Dnoe ") {
+            setSuccess(true);
+        } else {
+            setSuccess(false);
+        }
+
+    }
+
     return (
-        <Text>hello profile </Text>
+        <>
+            <SafeAreaView style={{ flex: 1, backgroundColor: "#10bbb3" }}>
+                <StatusBar barStyle={'light-content'} />
+
+                <View style={styles.container} >
+                    <Header />
+
+                    <Text style={styles.title}>Send a constructive message </Text>
+                    <TextInput
+                        selectionColor={'#10bbb3'}
+                        style={styles.textarea}
+                        multiline={true}
+                        onChangeText={(text) => setMessage(text)}
+                    />
+                    <IconButton text='Send' icon={faPen} onPress={() => handleSend()} />
+                </View >
+            </SafeAreaView >
+
+            {success ? <Modal onHide={() => { setSuccess(false); navigation.navigate('Users') }} style={styles.modal} title="Congratulations 🎉" subTitle='Message Sent Successfully' /> : null}
+        </>
     );
 }
+
+const styles = StyleSheet.create({
+
+    container: {
+        backgroundColor: '#eff2f5',
+        flex: 1,
+    },
+    title: {
+        fontSize: 22,
+        marginTop: 20,
+        color: '#10bbb3',
+        textAlign: 'center',
+        fontWeight: '700',
+    },
+    textarea: {
+        height: '50%',
+        margin: 30,
+        backgroundColor: 'yellow',
+        padding: 20,
+        paddingTop: 20,
+        fontSize: 20,
+        borderWidth: 1,
+        borderColor: '#10bbb3',
+        backgroundColor: '#fff',
+        fontWeight: '500',
+        borderRadius: 10,
+    },
+    send: {
+        marginLeft: "auto",
+        border: "none",
+        outline: "none",
+        padding: 0,
+        backgroundColor: "transparent",
+    },
+})
--- saraha/src/screens/Users.jsx
+++ saraha/src/screens/Users.jsx
@@ -1,25 +1,32 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios'
-import { Home } from '../screens';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Home } from "../screens";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons/faPaperPlane";
 import { faSearch } from "@fortawesome/free-solid-svg-icons/faSearch";
 
 import { faUserCircle } from "@fortawesome/free-solid-svg-icons/faUserCircle";
-import { Text, StyleSheet, View, ScrollView, TextInput } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import {
+    Text,
+    StyleSheet,
+    View,
+    ScrollView,
+    TextInput,
+    SafeAreaView,
+    StatusBar,
+    TouchableOpacity,
+} from "react-native";
+
+import { Header } from "../components";
 
 export const Users = ({ navigation }) => {
     const [users, setUsers] = useState([]);
     const [searchField, setSearchField] = useState("");
     const [filteredUser, setFilteredUser] = useState();
 
-
     //get data from api
     const getAllUsers = async () => {
-        let { data } = await axios.get(
-            "http://localhost:3000/api/v1/auth/allusers"
-        );
+        let { data } = await axios.get("http://10.0.2.2:3000/api/v1/auth/allusers");
         setUsers(data.users);
     };
 
@@ -37,91 +44,108 @@ export const Users = ({ navigation }) => {
     }, [users, searchField]);
 
     return (
-        <View style={styles.container} >
-            <View style={styles.inputContainer}>
-                <FontAwesomeIcon size={20} icon={faSearch} color="#10bbb3" />
-                <TextInput
-                    onChangeText={(text) => setSearchField(text)}
-                    style={styles.search}
-                    className="search"
-                    type="search"
-                    placeholder="Search user name "
-                />
-            </View>
-            <ScrollView showsVerticalScrollIndicator={false} >
-                {filteredUser?.map((user, index) => {
-                    return (
-                        <View style={styles.userContainer} >
-                            <FontAwesomeIcon size={30} icon={faUserCircle} color="#10bbb3" />
-                            <View style={styles.details}>
-                                <Text style={styles.name}>{user.userName}</Text>
-                                <Text style={styles.email}>{user.email}</Text>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "#10bbb3" }}>
+            <StatusBar barStyle={"light-content"} />
 
+            <View style={styles.container}>
+                <Header />
+                <View style={styles.inputContainer}>
+                    <FontAwesomeIcon size={20} icon={faSearch} color="#10bbb3" />
+                    <TextInput
+                        onChangeText={(text) => setSearchField(text)}
+                        style={styles.search}
+                        className="search"
+                        type="search"
+                        placeholder="Search user name "
+                    />
+                </View>
+                <ScrollView showsVerticalScrollIndicator={false}>
+                    {filteredUser?.map((user, index) => {
+                        return (
+                            <View style={styles.userContainer} key={index}>
+                                <FontAwesomeIcon
+                                    size={30}
+                                    icon={faUserCircle}
+                                    color="#10bbb3"
+                                />
+                                <View style={styles.details}>
+                                    <Text style={styles.name}>{user.userName}</Text>
+                                    <Text style={styles.email}>{user.email}</Text>
+                                </View>
+                                <TouchableOpacity
+                                    style={styles.send}
+                                    onPress={() => {
+                                        console.log("hello");
+                                        navigation.navigate("SendMessage", {
+                                            id: user._id,
+                                            name: user.userName,
+                                        });
+                                    }}
+                                >
+                                    <FontAwesomeIcon
+                                        size={20}
+                                        icon={faPaperPlane}
+                                        color="#10bbb3"
+                                    />
+                                </TouchableOpacity>
                             </View>
-                            <TouchableOpacity style={styles.send} onPress={() => navigation.navigate('Home', {
-                                screen: 'SendMessage',
-                                // params: { user: 'jane' },
-                            })}
-                            >
-                                <FontAwesomeIcon size={20} icon={faPaperPlane} color="#10bbb3" />
-                            </TouchableOpacity>
-                        </View>
-                    )
-                })}
-            </ScrollView>
-        </View>
+                        );
+                    })}
+                </ScrollView>
+            </View>
+        </SafeAreaView>
     );
-}
+};
 
 const styles = StyleSheet.create({
-
     container: {
-        backgroundColor: '#eff2f5',
-        padding: 20,
+        backgroundColor: "#eff2f5",
+        paddingBottom: 0,
+        flex: 1,
     },
     userContainer: {
-
         display: "flex",
-        flexDirection: 'row',
+        flexDirection: "row",
         flex: 1,
         alignItems: "center",
         padding: 15,
-        marginBottom: 20,
+        margin: 20,
+        marginTop: 0,
         borderRadius: 10,
         borderWidth: 1,
-        borderColor: '#10bbb3',
-        backgroundColor: '#fff'
+        borderColor: "#10bbb3",
+        backgroundColor: "#fff",
     },
     details: {
-        disply: 'flex',
-        flexDirection: 'column',
+        disply: "flex",
+        flexDirection: "column",
         marginLeft: 15,
-        marginRight: 'auto'
+        marginRight: "auto",
     },
     send: {
         marginLeft: "auto",
         border: "none",
         outline: "none",
-        padding: 0,
-        backgroundColor: "transparent",
+        padding: 20,
     },
     name: {
         fontSize: 20,
-        textTransform: 'capitalize'
+        textTransform: "capitalize",
     },
     email: {
-        color: 'gray'
+        color: "gray",
     },
     inputContainer: {
         display: "flex",
-        flexDirection: 'row',
-        width: "100%",
+        flexDirection: "row",
+        width: "90%",
         backgroundColor: "#fff",
-        marginBottom: 20,
+        margin: 20,
         borderRadius: 10,
-        alignItems: "center",
         overflow: "hidden",
         padding: 10,
+        justifyContent: "center",
+        alignItems: "center",
     },
     search: {
         border: "none",
@@ -131,6 +155,5 @@ const styles = StyleSheet.create({
         width: "90%",
         fontWeight: "500",
         marginLeft: 20,
-
     },
-})
+});
