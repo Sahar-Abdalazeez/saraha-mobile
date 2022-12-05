db83cbd060fd2758c78a9527b0d4054111154b86
sahar <saharabdalazeez97@gmail.com>
2022-12-05 23:15:29 +0200

    adding components and animation json files

--- saraha/src/components/index.js
+++ saraha/src/components/index.js
@@ -3,5 +3,17 @@ import {Screen} from './Screen';
 import {Logo} from './Logo';
 import {MainButton} from './MainButton';
 import {NavigationButton} from './NavigationButton';
+import {Header} from './Header';
+import {IconButton} from './IconButton';
+import {Modal} from './Modal';
 
-export {Input, Screen, Logo, MainButton, NavigationButton};
+export {
+  Input,
+  Screen,
+  Logo,
+  MainButton,
+  NavigationButton,
+  Header,
+  IconButton,
+  Modal,
+};
