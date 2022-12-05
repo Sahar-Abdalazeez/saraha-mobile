54f5bb4aefd846904c47f41b08ad5ce51d29c8c9
sahar <saharabdalazeez97@gmail.com>
2022-12-05 23:14:44 +0200

    modal ready with lottie

--- saraha/src/components/Modal.jsx
+++ saraha/src/components/Modal.jsx
@@ -1,16 +1,27 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import LottieView from 'lottie-react-native';
 
-import { faPen } from "@fortawesome/free-solid-svg-icons/faPen";
+import { faX } from "@fortawesome/free-solid-svg-icons/faX";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons/faCheckCircle";
 
-import { Text, StyleSheet, View, TextInput, SafeAreaView, StatusBar } from 'react-native';
-import { Header, IconButton } from '../components';
+import { Text, StyleSheet, View, TextInput, SafeAreaView, Button, TouchableOpacity } from 'react-native';
+import { Header, IconButton, MainButton } from '../components';
 
 
-export const Modal = () => {
+export const Modal = ({ onContinuePressed, title, subTitle, onHide, animation }) => {
     return (
         <View style={styles.modal}>
-            <View style={styles.container} ></View>
+            <View style={styles.container} >
+                <TouchableOpacity style={styles.button} onPress={() => onHide()} >
+                    <FontAwesomeIcon icon={faX} />
+                </TouchableOpacity>
+                <Text style={styles.title}>{title}</Text>
+                <Text style={styles.subTitle}>{subTitle}</Text>
+                <LottieView style={{ alignSelf: 'center', width: 200, height: 200 }} source={animation} autoPlay loop />
+                <MainButton text="Continue" style={styles.mainButton} onPress={() => onContinuePressed()} />
+            </View>
         </View>
     )
 }
@@ -22,14 +33,47 @@ const styles = StyleSheet.create({
         width: '100%',
         position: 'absolute',
         zIndex: 1,
-        // marginTop: 'auto'
 
     },
     container: {
         width: '100%',
-        height: '70%',
+        height: '80%',
         marginTop: 'auto',
-        backgroundColor: '#fff'
+        backgroundColor: '#fff',
+        padding: 10,
+        paddingBottom: 30,
+        borderTopEndRadius: 30,
+        borderTopStartRadius: 30,
+    },
 
+    title: {
+        fontWeight: '600',
+        fontSize: 30,
+        color: '#10bbb3',
+        marginTop: 20,
+        textAlign: 'center'
+    },
+    subTitle: {
+        fontWeight: '500',
+        fontSize: 26,
+        color: '#10bbb3',
+        textAlign: 'center',
+        marginBottom: 10,
+
+    },
+
+    button: {
+        padding: 5,
+        alignSelf: 'baseline',
+        marginLeft: 'auto',
+
+    },
+    mainButton: {
+        borderWidth: 1,
+        borderColor: '#10bbb3',
+    },
+    buttonContainer: {
+        marginTop: 'auto',
+        paddingBottom: 20,
     }
 })
